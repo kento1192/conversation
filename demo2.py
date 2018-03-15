981b317e13c62f89e84c40e01adc84ada388948b
kento1192 <37197374+kento1192@users.noreply.github.com>
2018-03-15 04:17:42 +0000

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -3,6 +3,9 @@
 from choiseQuestion import *
 import sys
 
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 cq = CQ_RandomForest(sys.argv[1], sys.argv[2])
 rq = cq.recomendSimilarQuestion(sys.argv[3])
 
